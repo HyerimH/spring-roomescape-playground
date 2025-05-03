1076ad76587cc5925b71e7eb7a703089e4f7d98e
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-03 16:24:00 +0900

    feat: jpa 어노테이션 삭제

--- src/main/java/roomescape/entity/Reservation.java
+++ src/main/java/roomescape/entity/Reservation.java
@@ -1,30 +1,41 @@
 package roomescape.entity;
 
-import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@Entity
 public class Reservation {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+    private Long id;
 
-  private String name;
+    private String name;
 
-  private LocalDate date;
+    private LocalDate date;
 
-  private LocalTime time;
+    private LocalTime time;
 
-  public Reservation(Long id, String name, LocalDate date, LocalTime time) {
-    this.id = id;
-    this.name = name;
-    this.date = date;
-    this.time = time;
-  }
+    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
 
-  public Reservation() {
+    public Reservation() {
+    }
 
-  }
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
 }
