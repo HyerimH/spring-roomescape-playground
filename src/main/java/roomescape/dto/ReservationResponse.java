f64863a118a7c3538c1a3ad87f90dee60f278b63
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-07 22:33:11 +0900

    feat: @Getter, final 추가

--- src/main/java/roomescape/dto/ReservationResponse.java
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -2,37 +2,23 @@ package roomescape.dto;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import lombok.Getter;
 
+@Getter
 public class ReservationResponse {
 
-    private Long id;
+    private final Long id;
 
-    private String name;
+    private final String name;
 
-    private LocalDate date;
+    private final LocalDate date;
 
-    private LocalTime time;
+    private final LocalTime time;
 
     public ReservationResponse(Long id, ReservationRequest request) {
         this.id = id;
         this.name = request.getName();
         this.date = request.getDate();
         this.time = request.getTime();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
     }
 }
