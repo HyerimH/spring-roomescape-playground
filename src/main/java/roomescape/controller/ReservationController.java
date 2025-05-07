7d297e45fbeb01de0d1535ff949bebe389d6e22d
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-07 19:14:10 +0900

    feat: step3 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,37 +1,62 @@
 package roomescape.controller;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import jakarta.validation.Valid;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.domain.Reservation;
+import roomescape.dto.ReservationRequest;
+import roomescape.dto.ReservationResponse;
 
 @Controller
 public class ReservationController {
 
-  private List<Reservation> reservations = new ArrayList<>();
+    private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(0);
 
-  public ReservationController() {
-    reservations.add(new Reservation(1L, "브라운", LocalDate.parse("2023-01-01"), LocalTime.parse("10:00")));
-    reservations.add(new Reservation(2L, "옐로우", LocalDate.parse("2023-01-02"), LocalTime.parse("11:00")));
-    reservations.add(new Reservation(3L, "레드", LocalDate.parse("2023-01-03"), LocalTime.parse("12:00")));
-  }
+    // 예약 관리 페이지 반환
+    @GetMapping("/reservation")
+    public String Reservation(Model model) {
+        model.addAttribute("reservations", reservations);
+        return "reservation";
+    }
 
-  // 예약 관리 페이지 반환
-  @GetMapping("/reservation")
-  public String Reservation(Model model) {
-    model.addAttribute("reservations", reservations);
-    return "reservation";
-  }
+    // 예약 목록 조회
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
 
-  // 예약 목록 조회
-  @GetMapping("/reservations")
-  @ResponseBody
-  public List<Reservation> getReservations() {
-    return reservations;
-  }
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<ReservationResponse> addReservation(@Valid @RequestBody ReservationRequest request) {
+        Long newId = index.incrementAndGet();
+        reservations.add(new Reservation(
+                newId,
+                request.getName(),
+                request.getDate(),
+                request.getTime()
+        ));
+        ReservationResponse response = new ReservationResponse(newId, request);
+        return ResponseEntity
+                .created(URI.create("/reservations/" + newId))
+                .body(response);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        reservations.removeIf(reservation -> reservation.getId().equals(id));
+        return ResponseEntity.noContent().build();
+    }
 }
