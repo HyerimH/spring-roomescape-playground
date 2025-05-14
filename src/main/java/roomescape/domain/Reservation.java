7c08f4f58b05bade8c7e5b66b65f903bd8bf7f0e
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-14 18:03:22 +0900

    feat: 순수 JDBC 구현
    feat: 순수 JDBC 구현

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -3,9 +3,11 @@ package roomescape.domain;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Objects;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Data
 @Getter
 @RequiredArgsConstructor
 public class Reservation {
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -3,26 +3,30 @@ package roomescape.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import roomescape.dao.ReservationDAO;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
 import roomescape.exception.NotFoundException;
 
 @Service
+@RequiredArgsConstructor
 public class ReservationService {
 
-    private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong index = new AtomicLong(0);
+    private final ReservationDAO reservationDAO;
+
 
     public List<Reservation> getAllReservations() {
-        return new ArrayList<>(reservations);
+        return reservationDAO.findAllReservations();
     }
 
     public ReservationResponse addReservation(ReservationRequest request) {
         Long newId = index.incrementAndGet();
         Reservation reservation = request.toReservation(newId);
-        reservations.add(reservation);
+        reservationDAO.addReservation(reservation);
         return new ReservationResponse(
                 reservation.getId(),
                 reservation.getName(),
@@ -32,9 +36,10 @@ public class ReservationService {
     }
 
     public void deleteReservation(Long id) {
-        boolean isDeleted = reservations.removeIf(reservation -> reservation.hasId(id));
-        if (!isDeleted) {
+        Reservation reservation = reservationDAO.findReservationById(id);
+        if (reservation == null) {
             throw new NotFoundException("해당 예약을 찾을 수 없습니다: " + id);
         }
+        reservationDAO.deleteReservation(id);
     }
 }
