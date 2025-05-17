442085cc7ddc3eb6cc706ed4308696f16bdc984a
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-18 00:48:12 +0900

    feat: step 56 피드백 반영

--- src/main/java/roomescape/dao/ReservationDAO.java
+++ src/main/java/roomescape/dao/ReservationDAO.java
@@ -1,5 +1,7 @@
 package roomescape.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -18,30 +20,32 @@ public class ReservationDAO {
     private final JdbcTemplate jdbcTemplate;
 
     public List<Reservation> findAllReservations() {
-        final var query = "SELECT id, name, date, time FROM RESERVATION";
+        String query = "SELECT id, name, date, time FROM RESERVATION";
 
-        List<Reservation> reservations = jdbcTemplate.query(query, (resultSet, rowNum) -> new Reservation(
-                resultSet.getLong("id"),
-                resultSet.getString("name"),
-                LocalDate.parse(resultSet.getString("date")),
-                LocalTime.parse(resultSet.getString("time"))
-        ));
+        List<Reservation> reservations = jdbcTemplate.query(query, (resultSet, rowNum) ->
+                mapRowToReservation(resultSet)
+        );
 
         return new ArrayList<>(reservations);
     }
 
     public Reservation  findReservationById(final Long id) {
-        final var query = "SELECT * FROM RESERVATION WHERE id = ?";
+        String query = "SELECT * FROM RESERVATION WHERE id = ?";
 
         try {
-            return jdbcTemplate.queryForObject(query, (resultSet, rowNum) -> new Reservation(
-                    resultSet.getLong("id"),
-                    resultSet.getString("name"),
-                    LocalDate.parse(resultSet.getString("date")),
-                    LocalTime.parse(resultSet.getString("time"))
-            ), id);
+            return jdbcTemplate.queryForObject(query, (resultSet, rowNum) ->
+                    mapRowToReservation(resultSet), id);
         } catch (EmptyResultDataAccessException e) {
             throw new NotFoundException("해당 Reservation을 찾을 수 없습니다. ID: " + id);
         }
+    }
+
+    private Reservation mapRowToReservation(ResultSet resultSet) throws SQLException {
+        return new Reservation(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                LocalDate.parse(resultSet.getString("date")),
+                LocalTime.parse(resultSet.getString("time"))
+        );
     }
 }
