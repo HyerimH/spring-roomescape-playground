9dee0d234f173bfe03daf54f65fb9d26d8a3471e
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-18 11:43:45 +0900

    refactor: mapRowToReservation 변경

--- src/main/java/roomescape/dao/ReservationDAO.java
+++ src/main/java/roomescape/dao/ReservationDAO.java
@@ -1,7 +1,5 @@
 package roomescape.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -9,6 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import roomescape.domain.Reservation;
 import roomescape.exception.NotFoundException;
@@ -21,27 +20,22 @@ public class ReservationDAO {
 
     public List<Reservation> findAllReservations() {
         String query = "SELECT id, name, date, time FROM RESERVATION";
-
-        List<Reservation> reservations = jdbcTemplate.query(query, (resultSet, rowNum) ->
-                mapRowToReservation(resultSet)
-        );
-
+        List<Reservation> reservations = jdbcTemplate.query(query, mapRowToReservation());
         return new ArrayList<>(reservations);
     }
 
-    public Reservation  findReservationById(final Long id) {
+    public Reservation findReservationById(final Long id) {
         String query = "SELECT * FROM RESERVATION WHERE id = ?";
 
         try {
-            return jdbcTemplate.queryForObject(query, (resultSet, rowNum) ->
-                    mapRowToReservation(resultSet), id);
+            return jdbcTemplate.queryForObject(query, mapRowToReservation(), id);
         } catch (EmptyResultDataAccessException e) {
             throw new NotFoundException("해당 Reservation을 찾을 수 없습니다. ID: " + id);
         }
     }
 
-    private Reservation mapRowToReservation(ResultSet resultSet) throws SQLException {
-        return new Reservation(
+    private RowMapper<Reservation> mapRowToReservation() {
+        return (resultSet, rowNum) -> new Reservation(
                 resultSet.getLong("id"),
                 resultSet.getString("name"),
                 LocalDate.parse(resultSet.getString("date")),
