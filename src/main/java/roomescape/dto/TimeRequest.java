b533783b61069b6587adaee0cf15f3da23854642
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-05-21 17:21:49 +0900

    refactor: timedto 어노테이션 변경
    refactor: timedto 어노테이션 변경

--- src/main/java/roomescape/dto/TimeRequest.java
+++ src/main/java/roomescape/dto/TimeRequest.java
@@ -1,24 +1,19 @@
 package roomescape.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import roomescape.domain.Time;
 
 @Getter
+@NoArgsConstructor
 public class TimeRequest {
 
     @NotNull(message = "시간은 필수입니다.")
     @JsonFormat(pattern = "HH:mm")
-    private final LocalTime time;
-
-    @JsonCreator
-    public TimeRequest(@JsonProperty("time") LocalTime time) {
-        this.time = time;
-    }
+    private LocalTime time;
 
     public Time toTime() {
         return new Time(time);
